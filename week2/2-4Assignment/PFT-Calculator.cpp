bc967cc3030e83e2d70f22b287f1b6ea2456d243
Peter Tumulty <peter@vaultinnovation.com>
2022-06-28 21:41:55 -0400

    made another copy of file 2-4

---
+++ week2/2-4Assignment/PFT-Calculator.cpp
@@ -0,0 +1,39 @@
+/*
+ * Calculator.cpp
+ *
+ *  Date: June 28, 2022
+ *  Author: Peter F. Tumulty
+ **/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char statement[100];
+	int op1, op2;
+	char operation;
+	char answer='y';
+	while (answer =='y')
+	{
+		cout << "Enter expression" << endl;
+		cin >> op2 >> operation >> op1;
+		if (operation == '+') {
+			cout << op1 << " + " << op2 << " = " << op1 + op2 << endl;
+    }
+		if (operation == '-'){
+			cout << op1 << " - " << op2 << " = " << op1 - op2 << endl;
+    }
+		if (operation == '*'){
+			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl;
+    }
+		if (operation == '/') {
+			cout << op1 << " * " << op2 << " = " << op1 / op2 << endl;
+    }
+		cout << "Do you wish to evaluate another expression? " << endl;
+		cin >> answer;
+	}
+}
+
+
