bc5d9905c61b03832c264cea3b2f83cb1d2a7a25
ptums <ptumulty923@gmail.com>
2022-06-30 16:51:35 -0400

    finished week 4 assignment

--- week4/44Assignment/PFT-FahrenheitCelsius.cpp
+++ week4/44Assignment/PFT-FahrenheitCelsius.cpp
@@ -1,24 +1,50 @@
+/**
+ *
+ * Name: Peter F. Tumulty
+ * Date: June 30, 2022
+ *
+ *
+ **/
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <cmath>
 
-
 using namespace std;
-
 
 int main()
 {
 
   string city;
   int fDegree, cDegree;
-
   ifstream fTextFile;
-  fTextFile.open("FahrenheitTemperature.txt");
-
   ofstream cTextFile;
+
+  fTextFile.open("FahrenheitTemperature.txt");
   cTextFile.open("CelciusTemperature.txt");
 
-  cout << "Hello World!";
+  if(!fTextFile.is_open()) {
+    cout << "There was an error opening up FahrenheitTemperature.txt";
+    return 1;
+  }
+
+  if(!cTextFile.is_open()) {
+    cout << "There was an error opening up CelciusTemperature.txt";
+    return 1;
+
+  }
+
+  while(!fTextFile.fail()) {
+    fTextFile >> city;
+    fTextFile >> fDegree;
+    
+    cDegree = (int)round(((fDegree - 32) * 5)/(double)9);
+    cTextFile << city << " " << cDegree << endl;
+  }
+
+  fTextFile.close();
+  cTextFile.close();
+
   return 0;
 }
