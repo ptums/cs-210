e8d2de29dde5d293de60645971b17c0e04000b9a
Peter Tumulty <peter@vaultinnovation.com>
2022-07-21 23:07:17 -0400

    working on options 2

--- week7/PFTProject3/main.cpp
+++ week7/PFTProject3/main.cpp
@@ -48,6 +48,96 @@ Return:
 */
 int callIntFunc(string proc, string param)
 {
+//    char *procname = new char[proc.length() + 1];
+//    std::strcpy(procname, proc.c_str());
+//
+//    char *paramval = new char[param.length() + 1];
+//    std::strcpy(paramval, param.c_str());
+//
+//
+//    PyObject *pName, *pModule, *pDict, *pFunc, *pValue = nullptr, *presult = nullptr;
+//    // Initialize the Python Interpreter
+//    Py_Initialize();
+//    // Build the name object
+//    PyRun_SimpleString("import sys");
+//    PyRun_SimpleString("import os");
+//    PyRun_SimpleString("sys.path.append(os.getcwd())");
+//
+//
+//
+//    pName = PyUnicode_FromString((char*)"python_modules.PythonCode");
+//    // Load the module object
+//    pModule = PyImport_Import(pName);
+//    // pDict is a borrowed reference
+//    pDict = PyModule_GetDict(pModule);
+//    // pFunc is also a borrowed reference
+//    pFunc = PyDict_GetItemString(pDict, procname);
+//    if (PyCallable_Check(pFunc))
+//    {
+//        pValue = Py_BuildValue("(z)", paramval);
+//        PyErr_Print();
+//        presult = PyObject_CallObject(pFunc, pValue);
+//        PyErr_Print();
+//    }
+//    else
+//    {
+//        PyErr_Print();
+//    }
+//    //printf("Result is %d\n", _PyLong_AsInt(presult));
+//    Py_DECREF(pValue);
+//    // Clean up
+//    Py_DECREF(pModule);
+//    Py_DECREF(pName);
+//    // Finish the Python Interpreter
+//    Py_Finalize();
+//
+//    // clean
+//    delete[] procname;
+//    delete[] paramval;
+//
+//
+//    return _PyLong_AsInt(presult);
+//    char *procname = new char[proc.length() + 1];
+//    std::strcpy(procname, proc.c_str());
+//
+//    PyObject *pName, *pModule, *pDict, *pFunc, *pValue = nullptr, *presult = nullptr;
+//    // Initialize the Python Interpreter
+//    Py_Initialize();
+//    // Build the name object
+//    PyRun_SimpleString("import sys");
+//    PyRun_SimpleString("import os");
+//    PyRun_SimpleString("sys.path.append(os.getcwd())");
+//
+//    pName = PyUnicode_FromString((char*)"python_modules.PythonCode");
+//    // Load the module object
+//    pModule = PyImport_Import(pName);
+//    // pDict is a borrowed reference
+//    pDict = PyModule_GetDict(pModule);
+//    // pFunc is also a borrowed reference
+//    pFunc = PyDict_GetItemString(pDict, procname);
+//    if (PyCallable_Check(pFunc))
+//    {
+//        pValue = Py_BuildValue("(i)", param);
+//        PyErr_Print();
+//        presult = PyObject_CallObject(pFunc, pValue);
+//        PyErr_Print();
+//    }
+//    else
+//    {
+//        PyErr_Print();
+//    }
+//    //printf("Result is %d\n", _PyLong_AsInt(presult));
+//    Py_DECREF(pValue);
+//    // Clean up
+//    Py_DECREF(pModule);
+//    Py_DECREF(pName);
+//    // Finish the Python Interpreter
+//    Py_Finalize();
+//
+//    // clean
+//    delete[] procname;
+//
+//    return _PyLong_AsInt(presult);
     char *procname = new char[proc.length() + 1];
     std::strcpy(procname, proc.c_str());
 
--- week7/PFTProject3/python_modules/PythonCode.py
+++ week7/PFTProject3/python_modules/PythonCode.py
@@ -2,16 +2,37 @@ import re
 import string
 import os
 
-def ReadItemsPurchased():
+def get_file_contents():
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'CS210_Project_Three_Input_File.txt')
     file = open(filename)
-    list = file.read().split()
-    sort_list = sorted(set(list))
-
-    for item in sort_list:
-        print(item, " : ", list.count(item), "\n")
+    item_list = file.read().split()
     file.close()
+
+    return item_list
+
+def print_results(item, count):
+    return "{}: {} \n".format(item, count)
+
+def strip_word(w):
+    return w.lower().strip()
+
+def ReadItemsPurchased():
+    item_list = get_file_contents()
+
+    for item in item_list:
+        print(print_results(item, item_list.count(item)))
+
+def find_item(word):
+    item_list = get_file_contents()
+
+    for item in item_list:
+        if strip_word(item) == strip_word(word):
+            print(print_results(item, item_list.count(item)))
+            break
+
+
+
 
 
 
